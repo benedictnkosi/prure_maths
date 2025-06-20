d9a2bc7965d6a2afd3ffb4fd349690a388a6d364
nkosibenedict <nkosi.benedict@gmail.com>
2025-06-20 13:23:44 +0200

    Update API and HOST URLs to production settings in api.ts

--- config/api.ts
+++ config/api.ts
@@ -4,9 +4,9 @@
 export const IMAGE_BASE_URL = `https://examquiz.dedicated.co.za/public/learn/learner/get-image?image=`;
 
 //prod
-// export const API_BASE_URL = 'https://examquiz.dedicated.co.za/public/learn';
-// export const HOST_URL = 'https://examquiz.dedicated.co.za';
+export const API_BASE_URL = 'https://examquiz.dedicated.co.za/public/learn';
+export const HOST_URL = 'https://examquiz.dedicated.co.za';
 
 //local
-export const API_BASE_URL = 'http://192.168.1.38:8000/public/learn';
-export const HOST_URL = 'http://192.168.1.38:8000';
+// export const API_BASE_URL = 'http://192.168.1.38:8000/public/learn';
+// export const HOST_URL = 'http://192.168.1.38:8000';
